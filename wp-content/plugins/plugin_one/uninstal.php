8eebc88818ecba9d76470709cd9af143d10800f6
Celso Andre <celsolog2012@gmail.com>
2016-09-27 11:56:56 -0300

    desisntalando o plugin

---
+++ wp-content/plugins/plugin_one/uninstal.php
@@ -0,0 +1,14 @@
+<?php
+
+if(!defined('ABSPATH')&& !defined('WP_UNISTALL_PLUGIN'))
+    exit();
+
+delete_option('pone_settingd');
+
+//Remover as Tabelas
+
+
+//remover uploads
+
+
+//remover o que for necessario
