c632c6ba76030e112ac1e534bb61adce5e40f7e7
Celso Andre <celsolog2012@gmail.com>
2016-09-27 10:21:31 -0300

    explicação sobre filters

--- wp-content/plugins/plugin_one/plugin_one.php
+++ wp-content/plugins/plugin_one/plugin_one.php
@@ -19,3 +19,25 @@ require_once(PONE_DIR.'admin.php');
 /*
     add_action('action_name', 'function', 'prioridade');
 */
+
+//Necessario utilizar a função wp_footer()
+add_action('wp_footer', 'pone_google_tracking_code');
+
+function pone_google_tracking_code(){
+    ?>
+        <!-- Código do google Analytics -->
+    <?php
+}
+
+/*Sobre Filtros*/
+add_filter('the_content', 'pone_alter_the_content');
+
+function pone_alter_the_content($content){
+    return $content . "<br/><p>Todos os direitos reservados a Celso André</p> ";
+}
+
+add_filter('the_title', 'pone_custom_title');
+
+function pone_custom_title($title){
+    return $title ." - por Celso André";
+}
