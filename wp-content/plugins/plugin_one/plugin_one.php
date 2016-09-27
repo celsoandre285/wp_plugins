a2f0622fae702dcdc618deac2b7379039c3b2bde
Celso Andre <celsolog2012@gmail.com>
2016-09-27 08:56:57 -0300

    criando menu no dashbord e pagina Hello World

--- wp-content/plugins/plugin_one/plugin_one.php
+++ wp-content/plugins/plugin_one/plugin_one.php
@@ -1,6 +1,34 @@
+<?php
+/*
 Plugin Name: Plugin One
 Description: Meu primeiro plugin
 Author: Celso André
 Version: 1.0
 Author URI: https://github.com/celsoandre285/wp_plugins
 */
+
+/*Adicionando e registrando um novo menu*/
+add_action('admin_menu', 'pone_register_menus');
+
+/*Criando um novo menu no dashbord do wordpress*/
+function pone_register_menus(){
+    add_options_page('Plugin One',      /*titulo da pagina*/
+                     'Hello World',     /*Titulo do menu*/
+                     'manage_options',  /*permissão de visualização*/
+                     'pone_hello_page', /*identificador unico - Slug*/
+                     'pone_render_page' /*função de renderizar pagina*/
+                     );
+}
+
+/*Função para renderizar pagina dentro do menu*/
+function pone_render_page(){
+    ?>
+        <div class="wrap">
+            <h2>Hello World</h2>
+            <p class="description">Seja bem vindo ao Painel do Wordpress</p>
+        </div>
+    <?php
+}
+
+
+
